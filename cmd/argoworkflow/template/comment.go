0b817c5ef25ad9f295eb0117134fe70069c82089
rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-01-16 21:27:33 +0800

    feat: support to show the outputs in the pr comment
    Signed-off-by: rick <1450685+LinuxSuRen@users.noreply.github.com>

--- cmd/argoworkflow/template/comment.go
+++ cmd/argoworkflow/template/comment.go
@@ -2,11 +2,24 @@ package template
 
 // CommentTemplate is the default comment template
 const CommentTemplate = `
-[{{.Spec.WorkflowTemplateRef.Name}}]({{get .Annotations "workflow.templatelink"}}) is {{.Status.Phase}}. It takes {{duration .Status.FinishedAt .Status.StartedAt}}. Please check log output from [here]({{get .Annotations "workflow.link"}}).
+[{{.Spec.WorkflowTemplateRef.Name}}]({{get .Annotations "workflow.templatelink"}}) is {{.Status.Phase}}. It started from {{date "01-02 15:04" .Status.StartedAt.Time}}, and took {{duration .Status.FinishedAt .Status.StartedAt}}. Please check log output from [here]({{get .Annotations "workflow.link"}}).
 
 | Stage | Status | Duration |
 |---|---|---|
 {{- range $_, $status := .Status.Nodes}}
 | {{$status.DisplayName}} | {{$status.Phase}} | {{duration $status.FinishedAt $status.StartedAt}} |
+{{- end}}
+`
+
+const OutputsTemplate = `
+Please feel free to check the following outputs:
+| Output |
+|---|
+{{- range $name, $output := .}}
+{{- if eq "file" (toString $output.Kind)}}
+| [{{$name}}]({{$output.File}}) |
+{{- else if eq "string" (toString $output.Kind)}}
+| {{$name}} - {{$output.Value}} |
+{{- end}}
 {{- end}}
 `
---
+++ cmd/argoworkflow/template/workflow-output.go
@@ -0,0 +1,19 @@
+package template
+
+// OutputObject represents a output object
+type OutputObject struct {
+	Kind  OutputObjectKind
+	File  string
+	Value string
+}
+
+// OutputObjectKind represents the type of the outout object.
+// This is a type alias of string.
+type OutputObjectKind string
+
+const (
+	// FileOutput represetnts a file path
+	FileOutput OutputObjectKind = "file"
+	// ValueOutput represents a string value
+	ValueOutput OutputObjectKind = "string"
+)
