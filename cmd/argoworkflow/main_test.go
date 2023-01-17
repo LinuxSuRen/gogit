046a030de890991e5e3eea345b1dbf82e3f591f6
rick <LinuxSuRen@users.noreply.github.com>
2023-01-17 17:28:45 +0800

    feat: remove all the retried nodes

--- cmd/argoworkflow/main_test.go
+++ cmd/argoworkflow/main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	wfv1 "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEmptyThen(t *testing.T) {
@@ -28,4 +30,19 @@ func TestEmptyThen(t *testing.T) {
 			assert.Equal(t, tt.expect, result, "not match with", i)
 		})
 	}
+}
+
+func TestDeleteRetryNodes(t *testing.T) {
+	wf := &wfv1.Workflow{
+		Status: wfv1.WorkflowStatus{
+			Nodes: map[string]wfv1.NodeStatus{},
+		},
+	}
+	wf.Status.Nodes["test"] = wfv1.NodeStatus{DisplayName: "test"}
+	wf.Status.Nodes["test-0"] = wfv1.NodeStatus{DisplayName: "test-(0)"}
+	wf.Status.Nodes["test-10"] = wfv1.NodeStatus{DisplayName: "test-(10)"}
+	wf.Status.Nodes["test-100"] = wfv1.NodeStatus{DisplayName: "test-(100)"}
+
+	deleteRetryNodes(wf)
+	assert.Equal(t, 1, len(wf.Status.Nodes))
 }
