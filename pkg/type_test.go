b7dacceb1fdc0a8486de35542f8f34d19db3aeaa
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2024-03-11 18:32:02 +0800

    chore: increase the retry times (#49)
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- pkg/type_test.go
+++ pkg/type_test.go
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2023-2024 Rick
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 package pkg
 
 import "testing"
@@ -25,8 +49,11 @@ func TestRepoInformation_getRepoPath(t *testing.T) {
 				Owner: tt.fields.Owner,
 				Repo:  tt.fields.Repo,
 			}
-			if got := r.getRepoPath(); got != tt.want {
-				t.Errorf("getRepoPath() = %v, want %v", got, tt.want)
+			if got := r.GetRepoPath(); got != tt.want {
+				t.Errorf("GetRepoPath() = %v, want %v", got, tt.want)
+			}
+			if got := r.String(); got != tt.want {
+				t.Errorf("String() = %v, want %v", got, tt.want)
 			}
 		})
 	}
